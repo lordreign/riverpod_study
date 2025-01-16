8e8d2e8c8b6e16febcf433d273b43c6d2fc2f005
jungsun park <jungsun.park@kwatercraft.com>
2025-01-16 17:41:57 +0900

    riverpod 공부 중

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_study/layout/default_layout.dart';
+import 'package:riverpod_study/screen/state_notifier_provider_screen.dart';
+import 'package:riverpod_study/screen/state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => StateProviderScreen()),
+              );
+            },
+            child: Text('StateProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (_) => StateNotifierProviderScreen()),
+              );
+            },
+            child: Text('StateNotifierProviderScreen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/state_notifier_provider_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_study/layout/default_layout.dart';
+import 'package:riverpod_study/model/shopping_item_model.dart';
+import 'package:riverpod_study/riverpod/state_notifier_provider.dart';
+
+class StateNotifierProviderScreen extends ConsumerWidget {
+  const StateNotifierProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<ShoppingItemModel> state = ref.watch(shoppingListProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: ListView(
+        children: state
+            .map(
+              (e) => CheckboxListTile(
+                title: Text(e.name),
+                value: e.hasBought,
+                onChanged: (_) {
+                  ref
+                      .read(shoppingListProvider.notifier)
+                      .toggleHasBought(name: e.name);
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
